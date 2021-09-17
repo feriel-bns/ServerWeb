24c8aeb769a1907e95111fc67f192d062853a468
feriel-bns <ferielmimi76@gmail.com>
2021-09-17 11:10:42 +0200

    Update server.php

--- server.php
+++ server.php
@@ -2,15 +2,10 @@
 define("URL", str_replace("index.php","",(isset($_SERVER['HTTPS'])? "https" : "http").
 "://".$_SERVER['HTTP_HOST'].$_SERVER["PHP_SELF"]));
 
- include_once '../../Database.php';
-  
 
-  // Instantiate DB & connect
-  $database = new Database();
-  $pdo = $database->connect();
 
 function authentification($user_name,$user_pass){
-
+ $pdo = getConnexion();
 $req = "select username,password from heroku_abde6316f609ed5.compte  where login like '$user_name' and  motdepasse like '$user_pass';";
 //$stmt = $pdo->prepare($req);
 //$stmt->execute();
@@ -77,7 +72,28 @@ function historiquePointageByEmploye($idUser){
     
 }
 
+function getConnexion(){
+    
+   // DB Params
+   $host = 'us-cdbr-east-04.cleardb.com';
+   $db_name = 'heroku_abde6316f609ed5';
+   $username = 'b4a84a4b9166f9';
+   $password = '8e08be0f';
+   $conn;
 
+  // DB Connect
+
+  $this->conn = null;
+
+  try { 
+    $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch(PDOException $e) {
+    echo 'Connection Error: ' . $e->getMessage();
+  }
+
+  return $this->conn;
+}
 
 
 
